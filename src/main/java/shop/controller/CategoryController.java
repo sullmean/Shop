49d8a509add3a404bf8ed2da96734fc154603035
danghuunghia <danghuunghia428@gmail.com>
2018-05-02 17:02:22 +0700

    omm

--- src/main/java/shop/controller/CategoryController.java
+++ src/main/java/shop/controller/CategoryController.java
@@ -3,9 +3,11 @@ package shop.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import shop.entities.Category;
 import shop.service.CategoryService;
 
 @Controller
@@ -16,6 +18,21 @@ public class CategoryController {
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String manage_category(ModelMap mm) {
+		mm.put("listCategory", categoryService.getAllcategory());
+		return "/admin/manager_category";
+	}
+
+	// add
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String insertCategory(ModelMap mm) {
+		mm.put("newCategory", new Category());
+		return "/admin/insert_category";
+	}
+
+	// lưu
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveCategory(ModelMap mm, @ModelAttribute(value = "newCategory") Category category) {
+		categoryService.insertCategory(category);
 		mm.put("listCategory", categoryService.getAllcategory());
 		return "/admin/manager_category";
 	}
